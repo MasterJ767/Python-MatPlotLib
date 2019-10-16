5d5344b2605ed526324ad6a0dea7c84ee20e5b4b
b9063758 <b9063758@newcastle.ac.uk>
2019-10-16 10:46:26 +0100

    Testing sort of works

--- uniplot/analysis.py
+++ uniplot/analysis.py
@@ -1,8 +1,19 @@
 from . import parse
+from . import cli
+from functools import reduce
 
-loc = "uniprot_receptor.xml.gz"
+def add(x,y):
+    return x + y
+
+def sequence_total(loc):
+    sequence_lengths = []
+    for record in parse.uniprot_seqrecords(loc):
+        sequence = len(record)
+        sequence_lengths.append(sequence)
+    return reduce(add,sequence_lengths)
 
 def average_len(records):
     """Returns the average len for records."""
-    for record in parse.uniprot_seqrecords(loc):
-        return len(record)/len(record.name)
+    record_total = len(records)
+    return sequence_total(cli.loc())/record_total
+

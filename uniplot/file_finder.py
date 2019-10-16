6a1fc15711ed35c465e8f9ee4c6d340a8571bc6f
b9063758 <b9063758@newcastle.ac.uk>
2019-10-16 12:24:28 +0100

    Convention cleanup 2

--- uniplot/file_finder.py
+++ uniplot/file_finder.py
@@ -1,6 +1,8 @@
 import os
 
+
 def find():
+    """Find uniprot xml file"""
     if os.path.exists('uniprot_receptor.xml.gz') == True:
         file = os.path.abspath('uniprot_receptor.xml.gz')
         return os.path.relpath(file, '/uniprot/cli.py')

2c86618d6c81c5c23b8a65ae8bf4febdaa5ddbf9
b9063758 <b9063758@newcastle.ac.uk>
2019-10-17 11:08:29 +0100

    Testing works

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,7 +1,7 @@
 import uniplot.analysis
 import uniplot.parse
 
-TEST_UNIPROT = "./resource/uniprot_sprot_small.xml.gz"
+test_uniprot = "./resources/uniprot_sprot_small.xml.gz"
 
 
 def test_hello_world():
@@ -11,5 +11,6 @@ def test_hello_world():
 def test_average():
     """Test the average length function from analysis.py"""
     assert uniplot.analysis.average_len(
-        uniplot.parse.uniprot_seqrecords(TEST_UNIPROT)
-    ) == 302.722222222222222222222222222222222222
+        uniplot.parse.uniprot_seqrecords(test_uniprot)
+    ) == 302.72222222222223
+
